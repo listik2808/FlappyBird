88474b67acc7cff42675b97b93a2ef0f804a4351
Евгений <listopadov_94@mail.ru>
2023-08-23 14:39:17 +0300

    упарвления птичкой, слежение камерой за пичкой

---
+++ Assets/Scripts/Bird/BirdMover.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Berd
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BirdMover : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _tapForce = 10f;
+        [SerializeField] private float _rotationSpeed;
+        [SerializeField] private float _maxRotationZ;
+        [SerializeField] private float _minRotationZ;
+
+        private Vector3 _startPosition;
+        private Rigidbody2D _rigidbody2D;
+        private Quaternion _maxRotation;
+        private Quaternion _minRotation;
+
+        private void Start ()
+        {
+            _startPosition = transform.position;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _rigidbody2D.velocity = Vector2.zero;
+
+            _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+            _minRotation = Quaternion.Euler(0,0, _minRotationZ);
+        }
+
+        private void Update ()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                _rigidbody2D.velocity = new Vector2(_speed, 0);
+                transform.rotation = _maxRotation;
+                _rigidbody2D.AddForce(Vector2.up * _tapForce, ForceMode2D.Force);
+            }
+
+            transform.rotation = Quaternion.Lerp(transform.rotation,_minRotation,_rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/BirdTracker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class BirdTracker : MonoBehaviour
+    {
+        [SerializeField] private Bird _bird;
+        [SerializeField] private float _xOffset;
+
+        private void Update()
+        {
+            transform.position = new Vector3(_bird.transform.position.x - _xOffset ,transform.position.y,transform.position.z);
+        }
+    }
+}
